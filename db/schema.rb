3be3ec400f419f10ab758a0c8323825faca99cc1
dbarkwil <dbarkwil@iupui.edu>
2014-04-19 14:33:12 -0400

    Added Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411163447) do
+ActiveRecord::Schema.define(version: 20140419182254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 20140411163447) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistance_token"
+    t.string   "single_access_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
